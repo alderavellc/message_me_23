9561974762195905d9ce18942d8a5605aaa44a8f
alderavellc <alderavellc@gmail.com>
2021-03-01 01:52:11 -0700

    Comment out seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: "mhoelzel23@gmail.com", password: "2funky4u")
-User.create(email: "xinhtho@gmail.com", password: "2funky4u")
+# User.create(email: "mhoelzel23@gmail.com", password: "2funky4u")
+# User.create(email: "xinhtho@gmail.com", password: "2funky4u")
 
-Message.create(body: "Hi, Xinh!", user: User.first)
-Message.create(body: "Hi, Matt!", user: User.last)
-Message.create(body: "I love you!", user: User.first)
-Message.create(body: "I love you, too!", user: User.last)
+# Message.create(body: "Hi, Xinh!", user: User.first)
+# Message.create(body: "Hi, Matt!", user: User.last)
+# Message.create(body: "I love you!", user: User.first)
+# Message.create(body: "I love you, too!", user: User.last)
