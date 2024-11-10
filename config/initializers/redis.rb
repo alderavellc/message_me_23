e8cd13e320641e7b050a2d3c40ce85e1bb9f09f7
alderavellc <alderavellc@gmail.com>
2024-11-09 17:09:23 -0700

    Redis setup.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,13 @@
-# frozen_string_literal: true
+require 'redis'
 
-$redis = Redis.new(url: ENV['redis://:pae5b3853717a882328069a47a51a586a280966b7e5dc5446660ca8c42aa84837@ec2-34-237-221-193.compute-1.amazonaws.com:8699'])
+REDIS = Redis.new(
+  url: ENV['REDIS_TLS_URL'] || ENV['REDIS_URL']
+)
+
+# Optional: Log Redis status on startup
+begin
+  REDIS.ping # This tests if Redis is responding
+  Rails.logger.info "Connected to Redis successfully."
+rescue => e
+  Rails.logger.error "Redis connection failed: #{e.message}"
+end
