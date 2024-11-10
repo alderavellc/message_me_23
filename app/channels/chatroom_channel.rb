9476dbdc89737cd3e5dfa5547fe5c56b470b1eaf
alderavellc <alderavellc@gmail.com>
2024-11-09 18:07:02 -0700

    Redis more config, channel method.

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -6,4 +6,10 @@ class ChatroomChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def receive(data)
+    message = Message.create!(body: data["body"], user: current_user, chatroom: chatroom)
+    # Broadcasting the message after it's created
+    broadcast_to "chatroom_channel", mod_message: render_message(message)
+  end
 end
